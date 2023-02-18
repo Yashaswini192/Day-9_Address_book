8b49043c063c2b5d9bd1323412efcbed47a0b1fc
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-02-18 14:56:36 +0530

    add multiple persons to AddressBook

--- src/AB/AddressBookMain.java
+++ src/AB/AddressBookMain.java
@@ -5,10 +5,16 @@ import java.util.Scanner;
 public class AddressBookMain {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("enter the no of contacts you want to add:");
+		int totalContact = scan.nextInt();
 
 		AddressBook addressbook = new AddressBook();
-		addressbook.addContact();
-		System.out.println(addressbook.toString());
+
+		for(int i = 1 ; i <= totalContact ; i++) {
+			addressbook.addContact();
+			System.out.println(addressbook.toString());
+		}
 
 		addressbook.updateContact();
 		System.out.println(addressbook.toString());
